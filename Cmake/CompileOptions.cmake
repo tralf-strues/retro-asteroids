e0decf40a8f93a5f0305058005ab0d9aff47156c
Nikita Mochalov <nickita.mochalo@gmail.com>
2024-07-25 20:45:07 +0300

    Fix several compile errors & warnings x5 (thx apple clang again...)

--- Cmake/CompileOptions.cmake
+++ Cmake/CompileOptions.cmake
@@ -19,6 +19,7 @@ add_ra_compile_flags("-std=c++20")
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_ra_compile_flags("-stdlib=libc++")
+  add_ra_link_flags("-stdlib=libc++")
 endif()
 
 # Debug/Release
